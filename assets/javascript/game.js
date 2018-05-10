23d95d510f052091060a9e9e49d3997cc19b826b
Glasses <nrvpatel03@gmail.com>
2018-05-09 20:34:17 -0700

    worked on game almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,29 +19,85 @@ var genWords = ["apple", "broccoli", "carrot", "corn", "banana", "strawberry", "
     "asparagus", "mushroom", "watermelon", "pineapple", "cabbage", "tomato", "avocado",
     "peach", "kiwi", "mango", "cauliflower", "celery"
 ];
-//letter match, function that takes user letter, finds the index of the letter in the word and
-//change that space to a letter. if not (index of = -1) take away num guesses
-
+//letter match, function that takes user letter, finds all indexes of the letter in the word
+function letterMatch(userKey,randomWord){
+    var indexes = [];
+    for (var i = 0; i<randomWord.length; i++){
+        if(randomWord[i] === userKey){
+            indexes.push(i);
+        }
+    }
+    return indexes;
+}
+function replaceChar(userKey,indexes,wordSpace){
+    var splitArr = wordSpace.split(" ");
+    for(var i = 0; i<indexes.length; i++){
+        splitArr.splice(indexes[i],1,userKey);
+    }
+    return splitArr.join(" ");
+}
 
 // push button to start the game! space bar
 var wins = 0;
-var loss = 0;
 var numGuesses = 20;
-var randomWord = genRandoWord(genWords);
+var randomWord = "";
+var lettersGuessed = "";
 
 //start game setup everything
 document.onkeyup = function(event){
     var keyPressed = event.key;
     if(keyPressed === " "){
+        randomWord=genRandoWord(genWords);
         document.getElementById("wordSpaces").innerHTML = makeSpaces(randomWord);
-        document.getElementById("numWins").innerHTML = "Wins: " + wins + " Loss: " + loss;
-        document.getElementById("numGuessRem").innerHTML = "Number of Guesses Remaining: " + 20;
+        document.getElementById("numWins").innerHTML = "Wins: " + wins;
+        document.getElementById("numGuessRem").innerHTML = "Number of Guesses Remaining: " + numGuesses;
     }
 }
 //guess letters
 document.onkeydown = function(event){
+    var userKey = event.key;
+    var indexes = letterMatch(userKey,randomWord);
+    if(indexes.length>0){
+        //there is a match
+        document.getElementById("wordSpaces").innerHTML = replaceChar(userKey,indexes, document.getElementById("wordSpaces").innerHTML);
+        numGuesses -=1;
+        document.getElementById("numGuessRem").innerHTML = "Number of Guesses Remaining: " + numGuesses;
+        if(numGuesses>0){
+            //win
+            wins++;
+            console.log();
+            document.getElementById("numWins").innerHTML = "Wins: " + wins;
+            numGuesses = 20;
+            document.getElementById("wordSpaces").innerHTML = "You WIN! Press Spacebar for a new word";
+            lettersGuessed = "";
+            document.getElementById("lettersGuessed").innerHTML = "Letters Guessed " + lettersGuessed;
+        }else if(numGuesses<=0){
+            //lose
+            document.getElementById("wordSpaces").innerHTML = "Sorry you lose";
+            numGuesses = 20;
+            lettersGuessed = "";
+            document.getElementById("lettersGuessed").innerHTML = "Letters Guessed " + lettersGuessed;
+        }
+    }else{
+        //there isnt a match
+        lettersGuessed += userKey;
+        document.getElementById("lettersGuessed").innerHTML = "Letters Guessed " + lettersGuessed;
+        numGuesses -=1;
+        document.getElementById("numGuessRem").innerHTML = "Number of Guesses Remaining: " + numGuesses;
+        if(numGuesses<=0){
+            //lose
+            document.getElementById("wordSpaces").innerHTML = "Sorry you lose";
+            numGuesses = 20;
+            lettersGuessed = "";
+            document.getElementById("lettersGuessed").innerHTML = "Letters Guessed " + lettersGuessed;
+        }
+    }
+
+    
     
 }
 console.log(makeSpaces(randomWord));
 console.log(randomWord);
+console.log(letterMatch("a",randomWord));
+console.log(replaceChar("a",letterMatch("a",randomWord),makeSpaces(randomWord)));
 
